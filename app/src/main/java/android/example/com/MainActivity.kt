1a9ba8fd225bbd0d5fe7a86909d468619aa67da1
Mikhail <mla812@mail.ru>
2020-04-19 21:06:51 +0300

    Add comments in code

--- app/src/main/java/android/example/com/MainActivity.kt
+++ app/src/main/java/android/example/com/MainActivity.kt
@@ -9,6 +9,17 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 
+/**
+ * Main Activity of the AboutMe app. This app demonstrates:
+ *     * LinearLayout with TextViews, ImageView, Button, EditText, and ScrollView
+ *     * ScrollView to display scrollable text
+ *     * Getting user input with an EditText.
+ *     * Click handler for a Button to retrieve text from an EditText and set it in a TextView.
+ *     * Setting the visibility status of a view.
+ *     * Data binding between MainActivity and activity_main.xml. How to remove findViewById,
+ *       and how to display data in views using the data binding object.
+ */
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,6 +30,13 @@ class MainActivity : AppCompatActivity() {
             addNickname(it)
         }
     }
+
+    /**
+     * Click handler for the Done button.
+     * Demonstrates how data binding can be used to make code much more readable
+     * by eliminating calls to findViewById and changing data in the binding object.
+     */
+
     private fun addNickname(view: View){
         val editText = findViewById<EditText>(R.id.nickname_edit)
         val nicknameTextView = findViewById<TextView>(R.id.name_text)
